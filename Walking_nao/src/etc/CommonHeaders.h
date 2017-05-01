1a96017aa6da3322450f14809dc9a931449d8f49
EmileBahdi <emilebahdi@gmail.com>
2017-05-01 12:38:17 -0600

    Add files via upload

---
+++ Walking_nao/src/etc/CommonHeaders.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2016 nima@ua.pt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef COMMON_HEADERS_H
+#define COMMON_HEADERS_H
+
+/// \define some common headers which are used in most of the classes
+
+#include <cstring>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/joystick.h>
+#include <netinet/in.h>
+#include <vector>
+
+using namespace std;
+
+#endif
